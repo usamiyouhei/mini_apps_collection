32f4d130cb4c8411ca6716a2f5482b853e0128c4
youhei usami <usao9110@icloud.com>
2026-05-12 22:47:11 +0900

    5/12 card css

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import styles from "@/styles/appCard.module.scss";
 
 type AppCardProps = {
   title: string;
@@ -9,10 +9,10 @@ type AppCardProps = {
 
 export function AppCard({ title, description, href }: AppCardProps) {
   return (
-    <Link href={href}>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <span>Open App →</span>
+    <Link href={href} className={styles.card}>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.description}>{description}</p>
+      <span className={styles.next}>Open App →</span>
     </Link>
   );
 }
