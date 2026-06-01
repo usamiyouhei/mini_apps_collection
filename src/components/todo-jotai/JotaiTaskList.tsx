f14d841ed9dcba32df506f3568445371e7e8bb18
youhei usami <usao9110@icloud.com>
2026-06-01 12:11:28 +0400

    update TaskItem

--- src/components/todo-jotai/JotaiTaskList.tsx
+++ src/components/todo-jotai/JotaiTaskList.tsx
@@ -1,5 +1,22 @@
+"use client";
+import { Task } from "@/types/task";
+import styles from "./JotaiTaskList.module.scss";
 import React from "react";
+import JotaiTaskItem from "./JotaiTaskItem";
+type Props = {
+  tasks: Task[];
+};
 
-export default function JotaiTaskList() {
-  return <div></div>;
+export default function JotaiTaskList({ tasks }: Props) {
+  if (tasks.length === 0) {
+    return <p className={styles.empty}>タスクはまだありません。</p>;
+  }
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <JotaiTaskItem key={task.id} task={task} />
+      ))}
+    </ul>
+  );
 }
