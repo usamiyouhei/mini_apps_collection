489ee44214a890450060900a88dd1861697b7a90
youhei usami <usao9110@icloud.com>
2026-06-08 12:25:04 +0400

    update step components

---
+++ src/types/dessert.ts
@@ -0,0 +1,10 @@
+export type DessertIdea = {
+  id: string;
+  dessertTypes: string[];
+  flavors: string[];
+  textures: string[];
+  temperatures: string[];
+  decorations: string[];
+  memo?: string;
+  createdAt: string;
+};
