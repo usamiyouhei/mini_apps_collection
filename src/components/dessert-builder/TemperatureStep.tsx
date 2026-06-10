252faffad763b88c8d2df6de3d3413056df9728e
youhei usami <usao9110@icloud.com>
2026-06-10 10:05:30 +0400

    update Step

--- src/components/dessert-builder/TemperatureStep.tsx
+++ src/components/dessert-builder/TemperatureStep.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import OptionStep from "./OptionStep";
+import { temperatures } from "@/data/dessertOptions";
 
-export default function TemperatureStep() {
-  return <div></div>;
+type TemperatureStepProps = {
+  selectedValues: string[];
+  onToggle: (value: string) => void;
+};
+export default function TemperatureStep({
+  selectedValues,
+  onToggle,
+}: TemperatureStepProps) {
+  return (
+    <OptionStep
+      title="温度感"
+      options={temperatures}
+      selectedValues={selectedValues}
+      onToggle={onToggle}
+    />
+  );
 }
