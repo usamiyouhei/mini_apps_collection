3b4d1ae56f15e9b693c958ea6d87c82bedb7753b
youhei usami <usao9110@icloud.com>
2026-07-05 10:50:03 +0400

    update BuilderView
    props

---
+++ src/components/dessert-builder/views/ResultView.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ResultView() {
+  return <div></div>;
+}
---
+++ src/components/dessert-builder/views/SavedIdeasView.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SavedIdeasView() {
+  return <div></div>;
+}
