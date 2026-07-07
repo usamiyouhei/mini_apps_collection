3ce94052f6f5eafbe1c8c56df4d55e69dc94daea
youhei usami <usao9110@icloud.com>
2026-07-07 10:43:04 +0400

    update BuilderView

--- src/components/dessert-builder/views/ResultView.tsx
+++ src/components/dessert-builder/views/ResultView.tsx
@@ -1,5 +1,19 @@
+import { DessertIdea } from "@/types/dessert";
 import React from "react";
+import styles from "./ResultView.module.scss";
 
-export default function ResultView() {
-  return <div></div>;
+type ResultViewProps = {
+  result: DessertIdea;
+  aiPrompt: string;
+  onSave: () => void;
+  onReset: () => void;
+};
+
+export default function ResultView({
+  result,
+  aiPrompt,
+  onSave,
+  onReset,
+}: ResultViewProps) {
+  return <div className={styles.resultView}></div>;
 }
