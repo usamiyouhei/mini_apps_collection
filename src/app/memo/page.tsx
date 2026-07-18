9988d300e604ffac644b3d114f1502158414187e
youhei usami <usao9110@icloud.com>
2026-07-18 13:55:25 +0400

    update page.tsx

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+"use client";
 import styles from "./memo.module.scss";
+import { useMemos } from "@/hooks/useMemos";
 
 export default function MemoPage() {
+  const { memos, isLoading, error } = useMemos();
+
+  if (isLoading) {
+    return <p>読み込み中...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
   return (
-    <section>
-      <h1>Memo App</h1>
-      <p>メモアプリ</p>
-    </section>
+    <main className={styles.container}>
+      <h1 className={styles.mainTitle}>Memo App</h1>
+
+      {memos.length === 0 ? (
+        <p>メモはありません。</p>
+      ) : (
+        <ul className={styles.memoList}>
+          {memos.map((memo) => (
+            <li key={memo.id} className={styles.memoItem}>
+              <h2 className={styles.memoTitle}>{memo.title}</h2>
+              <p className={styles.memoBody}>{memo.body}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </main>
   );
 }
