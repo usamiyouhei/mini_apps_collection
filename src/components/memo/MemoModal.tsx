1652b41ba6069ee161ce2a2dc5aaf918d9a42cac
youhei usami <usao9110@icloud.com>
2026-07-23 11:24:56 +0400

    update MemoModal

--- src/components/memo/MemoModal.tsx
+++ src/components/memo/MemoModal.tsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 
 type MemoModalProps = {
   memo: Memo;
+  onEdit: (id: number, title: string, body: string) => Promise<void>;
+  onClose: () => void;
 };
 
-export default function MemoModal({ memo }: MemoModalProps) {
+export default function MemoModal({ memo, onEdit, onClose }: MemoModalProps) {
   const [title, setTitle] = useState(memo.title);
   const [body, setBody] = useState(memo.body);
   return <div></div>;
